028b4d7d5d735364dcf251f266a6880cf8991c62
Ignacio Arguello <yoelnacho@gmail.com>
2014-01-05 12:51:19 -0300

    first upload
    +gitignore rules update
    +add first folders

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem 'sass', '~> 3.2.12'
+gem 'compass', '~> 0.12.2'
+gem 'breakpoint', '~>1.3'
+gem 'sass-globbing', '~> 1.1.0'
---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt){
+
+	//levantar servidor
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	miappConfig = {
+		path: 'webapp/',
+	    cssDir: 'webapp/static/css',
+	    sassDir: 'webapp/static/sass',
+	    devUrl: 'miapp.vm',
+	    port: 5000
+	  };
+
+	grunt.initConfig({
+		webapp: miappConfig,
+
+		connect: {
+			server: {
+				options: {
+					port: '<%= webapp.port %>',
+					base: '<%= webapp.path %>'
+					//livereload: true
+					//keepalive: true //si utilizo livereload no hace falta que esté keepalive
+				}
+			}
+		},
+
+		watch: {   
+			options: {
+    			livereload: true,
+			},
+			all: {
+				files: '<%= webapp.path %>/index.html'
+			},   
+      		compass: {
+	        	files: ['<%= webapp.sassDir %>/**/*.{scss,sass}'],
+	        	tasks: ['compass:server'],
+	      	},
+	      	css: {
+	        	files: ['<%= webapp.sassDir %>/style.css'],
+	        	task:[]
+	      	}
+	    },
+
+		compass: {
+			options: {
+				require: ['sass-globbing', 'breakpoint'],
+				cssDir: '<%= webapp.cssDir %>',
+				sassDir: '<%= webapp.sassDir %>',
+				relativeAssets: true,
+				force: true
+			},
+			dist: {
+				options: {
+			  		outputStyle: 'compress',
+			  		environment: 'production'
+				}
+			},
+			server: {
+				options: {
+			  		outputStyle: 'expand',
+			  		noLineComments: true,
+			  		debugInfo: false
+				}
+			}
+		}
+
+
+	});
+
+	grunt.task.registerTask('default', ['connect', 'compass:server', 'watch']);
+};
